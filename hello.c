f27b7e740e29f62dff945694725b60942e876441
ingooyan <ingooyam@naver.com>
2016-06-14 20:23:19 -0700

    5개 모듈 수정 완료 2016.05.15

--- hello.c
+++ hello.c
@@ -1,13 +1,7 @@
 // hello.c
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-  char s1[10] = "hello";
-  char s2[10];
-  
-  strcpy(s1, s2);
-  
-  printf("%s\n, s2);
+  printf("hello\n");
 }
